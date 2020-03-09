1ee014016ed203f25d7986e44efd0aef5b4483df
Anibal Monteiro <anibalsmonteiro@gmail.com>
2020-03-09 11:28:21 -0300

    Primeiro controller e testes nas páginas razor

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Aplicativo WEB MVC de vendas";
+            ViewData["Email"] = "amonteiro@wi2.com.br";
 
             return View();
         }
