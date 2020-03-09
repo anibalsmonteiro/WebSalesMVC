b0924f21aae135db24d6ff1e29398daf81661eb3
Anibal Monteiro <anibalsmonteiro@gmail.com>
2020-03-09 17:29:10 -0300

    VendedorServico e BuscarTodos

--- SalesWebMvc/Controllers/VendedoresController.cs
+++ SalesWebMvc/Controllers/VendedoresController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class VendedoresController : Controller
     {
+
+        private readonly VendedorService _vendedorService;
+
+        public VendedoresController(VendedorService vendedorService)
+        {
+            _vendedorService = vendedorService;
+        }
+        
         public IActionResult Index()
         {
-            return View();
+            var list = _vendedorService.BuscarTodos();
+            return View(list);
         }
     }
 }
---
+++ SalesWebMvc/Services/VendedorService.cs
@@ -0,0 +1,25 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class VendedorService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public VendedorService (SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Vendedor> BuscarTodos()
+        {
+            return _context.Vendedor.ToList();
+        }
+
+    }
+}
